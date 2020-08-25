af9e5cd149b6d2fc7e6ac88e348f6a0f53ce0458
qucker135 <qucker135@gmail.com>
2020-08-25 13:35:16 +0200

    Delete unneccesary data structures

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # crosswords
 Crosswords generator
+
+For bigger dictionaries, check out /usr/share/dict/ contents or online sources.
--- crosswords.cpp
+++ crosswords.cpp
@@ -7,16 +7,16 @@ using namespace std;
 enum RC{ROW = true, COLUMN = false};
 
 class Record{
-	const int bx,by;
-	const bool Row;
+	const int bx,by; //coordinates of beginning
+	const bool Row;  //is record a ROW or a COLUMN?
 	const int length;
 	string content;
 public:
 	Record(int bx,int by,bool R,int l,string c):bx(bx),by(by),Row(R),length(l){
-		this->content = c;
+		content = c;
 	}
 	string getContent(){return content;}
-	void setContent(string c){this->content = c;}
+	void setContent(string c){content = c;}
 
 	bool getRow(){return Row;}
 	int getLength(){return length;}
@@ -24,146 +24,97 @@ public:
 	int getY(){return by;}
 };
 
-class Rules{
-	const int a,b,c,d;
-public:
-	Rules(int a,int b,int c,int d):a(a),b(b),c(c),d(d){}
-	int ga(){return a;}
-	int gb(){return b;}
-	int gc(){return c;}
-	int gd(){return d;}
-};
 
-//crossword scheme
-//#.#.#
-//.....
-//#....
-//##...
-//.....
-
-const int rozmiarX = 5, rozmiarY = 5;
-
-void wypisz(const int rozmiarX,const int rozmiarY,vector<Record>& rl){
-	for(int i=0;i<rozmiarX;i++){
-		for(int j=0;j<rozmiarY;j++){
-			bool czyOdpowiada = false;
+void show(vector<Record>& rl,const int sizeX,const int sizeY){
+	for(int i=0;i<sizeX;i++){
+		for(int j=0;j<sizeY;j++){
+			bool isLetter = false; //checks if coordinates (i,j) contain a letter or a hash (#)
 			for(vector<Record>::iterator it = rl.begin(); it!=rl.end(); it++){
 				switch(it->getRow()){
 					case ROW:
-						if(!czyOdpowiada && i==it->getX() && j>=it->getY() && j<it->getY()+it->getLength()){
+						if(!isLetter && i==it->getX() && j>=it->getY() && j<it->getY()+it->getLength()){
 							cout<<(it->getContent())[j-it->getY()];
-							czyOdpowiada = true;
+							isLetter = true;
 						}
 					break;
 					case COLUMN:
-						if(!czyOdpowiada && j==it->getY() && i>=it->getX() && i<it->getX()+it->getLength())
+						if(!isLetter && j==it->getY() && i>=it->getX() && i<it->getX()+it->getLength())
 						{
 							cout<<(it->getContent())[i-it->getX()];
-							czyOdpowiada = true;
+							isLetter = true;
 						}
 					break;
 				}
 			}
-			if(!czyOdpowiada) cout<<"#";
+			if(!isLetter) cout<<"#";
 		}
 		cout<<"\n";
 	}
+	cout<<"\n";
 }
 
-void gen(vector<Record>& rl,vector<Rules> rlz,int rozmiarX,int rozmiarY,int poziom=0){
+void gen(vector<Record>& rl,int sizeX,int sizeY){
 	if(rl[rl.size()-1].getContent()!=""){
-		wypisz(rozmiarX,rozmiarY,rl);
+		show(rl,sizeX,sizeY);
 	}
 	else{	
-		//znajdz pierwszy pusty element w liscie
-		/*vector<Record>::iterator it = rl.begin();
-		while(it->getContent()!=""){it++;}*/
+		//find the first empty element in the list
 		int q = 0;
 		while(rl[q].getContent()!=""){q++;}
-		//it wskazuje teraz na pierwszy pusty element
-		//otworz odpowiedni slownik
-		//int len = it->getLength();
+		//now q indicates on the first empty element
+		//open proper dictionary file
 		int len = rl[q].getLength();
-		ifstream slownik(to_string(len)+".txt");
-		if(! slownik.is_open()) cerr<<"Blad otwarcia slownika!"<<endl;
+		ifstream dict(to_string(len)+".txt");
+		if(! dict.is_open()) cerr<<"Error: opening dictionary failed!"<<endl;
 		else{
-			int debugInfo = 0;
 			string bfr = "";
-			while(slownik>>bfr){
-				cerr<<"Poziom: "<<poziom<<endl;
-				cerr<<"Dbg: "<<debugInfo<<endl;
-				bool czySpelnia = true;
-				for(vector<Rules>::iterator itz = rlz.begin(); itz!=rlz.end(); itz++){
-					int w1 = itz->ga();
-					int w2 = itz->gc();
+			while(dict>>bfr){//run through the dictionary
+				bool isAcceptable = true;//check if string bfr is acceptable
 				
-					int ind1 = itz->gb();
-					int ind2 = itz->gd();
+				//check all crossings with other records
 				
-					//jesli zasada itz dotyczy slowa rl[q]
-					if(w1==q){
-						if(rl[w2].getContent()!=""){
-							if(bfr[ind1]!=rl[w2].getContent()[ind2]) czySpelnia = false;
+				for(int i=0;i<rl.size();i++){
+					if(rl[q].getRow()!=rl[i].getRow() && rl[i].getContent()!=""){
+
+						if((rl[q].getRow()==COLUMN) * (rl[i].getX()>=rl[q].getX() && rl[i].getX()<rl[q].getX()+rl[q].getLength() && rl[q].getY()>=rl[i].getY() && rl[q].getY()<rl[i].getY()+rl[i].getLength()) + (rl[q].getRow()==ROW)*(rl[q].getX()>=rl[i].getX() && rl[q].getX()<rl[i].getX()+rl[i].getLength() && rl[i].getY()>=rl[q].getY() && rl[i].getY()<rl[q].getY()+rl[q].getLength())){//if rl[q] and rl[i] have a common element
+							
+							int indq = (rl[q].getRow()==COLUMN)*(rl[i].getX()-rl[q].getX()) + (rl[q].getRow()==ROW)*(rl[i].getY()-rl[q].getY());
+							int indi = (rl[q].getRow()==COLUMN)*(rl[q].getY()-rl[i].getY()) + (rl[q].getRow()==ROW)*(rl[q].getX()-rl[i].getX());
+							if(rl[i].getContent()[indi]!=bfr[indq]){ //if character does not match...
+								isAcceptable=false;break; 	 //...then it means bfr is not acceptable
+							}
+								
 						}
 					}
-					else if(w2==q){
-						if(rl[w1].getContent()!=""){
-							if(bfr[ind2]!=rl[w1].getContent()[ind1]) czySpelnia = false;
-						}	
-					}
-				}
-				cerr<<bfr<<endl;
-
-				if(czySpelnia){
+				}		
+				
+				if(isAcceptable){
 					rl[q].setContent(bfr);
-					gen(rl,rlz,rozmiarX,rozmiarY,poziom+1);
+					gen(rl,sizeX,sizeY);
 					rl[q].setContent("");
 				}
-				debugInfo++;
-				
 			}
 		}
 	}
 }
 
-int main(){
-	vector<Record> recordList;
-	//TEST1 - aaa
-	/*
-	recordList.push_back(Record(1,0,ROW,5,"aaaaa"));	//N1	0
-	recordList.push_back(Record(2,1,ROW,4,"aaaa"));	//N2	1
-	recordList.push_back(Record(3,2,ROW,3,"aaa"));	//N3	2
-	recordList.push_back(Record(4,0,ROW,5,"aaaaa"));	//N4	3
-	recordList.push_back(Record(0,1,COLUMN,3,"aaa"));	//Z1	4
-	recordList.push_back(Record(1,2,COLUMN,4,"aaaa"));	//Z2	5
-	recordList.push_back(Record(0,3,COLUMN,5,"aaaaa"));	//Z3	6
-	recordList.push_back(Record(1,4,COLUMN,4,"aaaa"));	//Z4	7
-	*/
-	//TEST2 - pusto
-	/*
-	recordList.push_back(Record(1,0,ROW,5,""));	//N1	0
-	recordList.push_back(Record(2,1,ROW,4,""));	//N2	1
-	recordList.push_back(Record(3,2,ROW,3,""));	//N3	2
-	recordList.push_back(Record(4,0,ROW,5,""));	//N4	3
-	recordList.push_back(Record(0,1,COLUMN,3,""));	//Z1	4
-	recordList.push_back(Record(1,2,COLUMN,4,""));	//Z2	5
-	recordList.push_back(Record(0,3,COLUMN,5,""));	//Z3	6
-	recordList.push_back(Record(1,4,COLUMN,4,""));	//Z4	7
-	*/
-	//TEST3 - nie uzywac, nie jest dobry
-	/*
-	recordList.push_back(Record(1,0,ROW,5,""));
-	recordList.push_back(Record(0,1,COLUMN,3,""));
-	recordList.push_back(Record(1,2,COLUMN,4,""));
-	recordList.push_back(Record(0,3,COLUMN,5,""));
-	recordList.push_back(Record(1,4,COLUMN,4,""));
-	recordList.push_back(Record(2,1,ROW,4,""));
-	recordList.push_back(Record(3,2,ROW,3,""));
-	recordList.push_back(Record(4,0,ROW,5,""));
-	*/
+int main(){	
+	vector<Record> recordList;	
 	
-	//TEST4
+	//TEST VALUES
 	
+	const int sizeX = 5, sizeY = 5;
+	
+	//test crossword scheme
+	
+	//   -Y->
+	//
+	//  #.#.#
+	//| #....
+	//X .....
+	//| #....
+	//v .....
+
 	recordList.push_back(Record(0,1,COLUMN,5,""));
 	recordList.push_back(Record(1,2,COLUMN,4,""));
 	recordList.push_back(Record(0,3,COLUMN,5,""));
@@ -173,66 +124,7 @@ int main(){
 	recordList.push_back(Record(3,1,ROW,4,""));
 	recordList.push_back(Record(4,0,ROW,5,""));
 	
-	vector<Rules> rulesList;
-	//TEST1 / TEST2
-	
-	/*rulesList.push_back(Rules(0,1,4,1));
-	rulesList.push_back(Rules(1,0,4,2));
-	rulesList.push_back(Rules(0,2,5,0));
-	rulesList.push_back(Rules(1,1,5,1));
-	rulesList.push_back(Rules(2,0,5,2));
-	rulesList.push_back(Rules(3,2,5,3));
-	rulesList.push_back(Rules(0,3,6,1));
-	rulesList.push_back(Rules(1,2,6,2));
-	rulesList.push_back(Rules(2,1,6,3));
-	rulesList.push_back(Rules(3,3,6,4));
-	rulesList.push_back(Rules(0,4,7,0));
-	rulesList.push_back(Rules(1,3,7,1));
-	rulesList.push_back(Rules(2,2,7,2));
-	rulesList.push_back(Rules(3,4,7,3));
-	*/	
-	//wypisz(rozmiarX,rozmiarY,recordList);
-
-	//TEST3 - wcale nie zoptymalizowany mylilem sie :P
-	/*
-	rulesList.push_back(Rules(0,1,1,1));
-	rulesList.push_back(Rules(0,2,2,0));
-	rulesList.push_back(Rules(0,3,3,1));
-	rulesList.push_back(Rules(0,4,4,0));
-	rulesList.push_back(Rules(5,0,1,2));
-	rulesList.push_back(Rules(5,1,2,1));
-	rulesList.push_back(Rules(5,2,3,2));
-	rulesList.push_back(Rules(5,3,4,1));
-	rulesList.push_back(Rules(6,0,2,2));
-	rulesList.push_back(Rules(6,1,3,3));
-	rulesList.push_back(Rules(6,2,4,2));
-	rulesList.push_back(Rules(7,2,2,3));
-	rulesList.push_back(Rules(7,3,3,4));
-	rulesList.push_back(Rules(7,4,4,3));
-	*/
-
-	//TEST4
-	
-	rulesList.push_back(Rules(0,1,4,0));
-	rulesList.push_back(Rules(1,0,4,1));
-	rulesList.push_back(Rules(2,1,4,2));
-	rulesList.push_back(Rules(3,0,4,3));
-	rulesList.push_back(Rules(0,2,5,1));
-	rulesList.push_back(Rules(1,1,5,2));
-	rulesList.push_back(Rules(2,2,5,3));
-	rulesList.push_back(Rules(3,1,5,4));
-	rulesList.push_back(Rules(0,3,6,0));
-	rulesList.push_back(Rules(1,2,6,1));
-	rulesList.push_back(Rules(2,3,6,2));
-	rulesList.push_back(Rules(3,2,6,3));
-	rulesList.push_back(Rules(0,4,7,1));
-	rulesList.push_back(Rules(1,3,7,2));
-	rulesList.push_back(Rules(2,4,7,3));
-	rulesList.push_back(Rules(3,3,7,4));
-
-
-	
-	gen(recordList,rulesList,rozmiarX,rozmiarY);
+	gen(recordList,sizeX,sizeY);
 
 	return 0;
 }
